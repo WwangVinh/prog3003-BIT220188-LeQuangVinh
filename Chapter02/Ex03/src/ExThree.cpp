b2ea534e58cf6d3cfdb87c87e907c81a993d1d21
WwangVinh <wwangvinh04@gmail.com>
2026-04-13 22:16:34 +0700

    Create ExThree.cpp

---
+++ Chapter02/Ex03/src/ExThree.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int soNguyen = 20;
+    float soThuc = 9.5;
+    char kyTu = 'V';
+    bool logic = true;
+
+    printf("%d, %.1f, %c, %d\n", soNguyen, soThuc, kyTu, logic);
+    return 0;
+}
